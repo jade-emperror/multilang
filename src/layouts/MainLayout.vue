ca7b010e27a0ec551d071d848965e0c84219dfdf
jade-emperror <maestrobharath@gmail.com>
2022-02-17 16:51:05 +0530

    multilang _ vue

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,17 +1,46 @@
 <template>
   <div class="q-pa-md">
     <div class="bottom row row-cols-5 wrap">
-      <div v-for="key in items" :key="key">
+
+      <!--
+        use to check resposne
+        dummy tab not used currently
+        [its just here for reference]
+       -->
+      <!-- <div v-for="key in items" :key="key">
         <div class="row items-start col" style="max-width:350px">
         <q-card class="my-card" style="min-width:250px" flat bordered>
           <q-img 
             src="res/banana.jpg" style="max-height:165px"
           />
           <q-card-section>
-            <div class="text-overline text-orange-9">{{ res_data[key].prod_class_ta }}</div>
-            <div class="text-h5 q-mt-sm q-mb-xs">{{ res_data[key].prod_name_ta }}</div>
+            <div class="text-overline text-orange-9">{{ res_data[key] }}</div>
+            <div class="text-h5 q-mt-sm q-mb-xs">{{ res_data[key] }}</div>
             <div class="text-caption text-grey">
-            {{ res_data[key].prod_desc_ta }}</div>
+            {{ res_data[key] }}</div>
+          </q-card-section>
+
+          <q-card-actions>
+            <q-btn flat color="green" label="Buy" />
+            <q-btn flat color="primary" label="Cart" />
+
+          </q-card-actions>
+
+        </q-card>
+      </div>
+      </div> -->
+
+      <div v-for="key in items" :key="key">
+        <div class="row items-start col" style="max-width:350px">
+        <q-card class="my-card" style="min-width:250px" flat bordered>
+          <q-img 
+            src="res/eggs.jpg" style="max-height:165px"
+          />
+          <q-card-section>
+            <div class="text-overline text-orange-9">{{ res_data[key].prod_id }}.{{ res_data[key].prod_class }}</div>
+            <div class="text-h5 q-mt-sm q-mb-xs">{{ res_data[key].prod_name }}</div>
+            <div class="text-caption text-grey">
+            {{ res_data[key].prod_desc }}</div>
           </q-card-section>
 
           <q-card-actions>
@@ -23,19 +52,18 @@
         </q-card>
       </div>
       </div>
+
     </div>
     <br>
     <div class="link-cont mt-auto">
       <q-item>
         <q-item-section>
           <div class="row" style="max-width:fit-content">
-          <q-btn ref="target" color="dark" push label="Test Button" value="ta" @click="getData()"></q-btn>
-
-          <q-btn ref="target" color="dark" push label="English" value="ta" @click="changeLocale('en')"></q-btn>
-          <q-btn ref="target" color="dark" push label="தமிழ்" value="ta" @click="changeLocale('ta')"></q-btn>
-          <q-btn ref="target" color="dark" push label="తెలుగు" value="ta" @click="changeLocale('te')"></q-btn>
-          <q-btn ref="target" color="dark" push label="ಕನ್ನಡ" value="ta" @click="changeLocale('ka')"></q-btn>
-          <q-btn ref="target" color="dark" push label="മലയാളം" value="ta" @click="changeLocale('ma')"></q-btn>
+          <q-btn ref="target" color="dark" push label="English" value="ta" @click="getData('en')"></q-btn>
+          <q-btn ref="target" color="dark" push label="தமிழ்" value="ta" @click="getData('ta')"></q-btn>
+          <q-btn ref="target" color="dark" push label="తెలుగు" value="ta" @click="getData('te')"></q-btn>
+          <q-btn ref="target" color="dark" push label="ಕನ್ನಡ" value="ta" @click="getData('ka')"></q-btn>
+          <q-btn ref="target" color="dark" push label="മലയാളം" value="ta" @click="getData('ma')"></q-btn>
           </div>
         </q-item-section>
       </q-item>
@@ -51,28 +79,30 @@ export default {
   data(){
       return{
         res_data:{
-          
         },
         items:[0,1,2,3,4],
       }
     },
+  mounted:function(){
+    this.getData('en');
+  },
   methods:{
-    changeLocale(lang_code){
-      this.$i18n.locale = lang_code;
-    },
-    getData(){
+    // changeLocale(lang_code){
+    //   this.$i18n.locale = lang_code;
+    // },
+    getData(lang_code){
       const axios = require('axios');
       var self = this;
-      axios.get('http://localhost:8000/api/getdata/?lang_code=ta')
+      var api_url = 'http://localhost:8000/api/getdata/?lang_code='+lang_code;
+      axios.get(api_url)
         .then(function (response) {
-          
           self.res_data = response.data;
-          console.log(self.res_data[0]["prod_id"])
+          console.log(api_url)
       })
         .catch(function (error) {
           console.log(error);
         })
     },
-  }
+  },
 }
 </script>
